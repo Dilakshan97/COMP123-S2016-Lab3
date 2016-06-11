9f2aa1c65982c34b8100fd80daa483296a1a68e5
Admin <Admin@ADMIN-PC>
2016-06-10 21:11:40 -0400

    Final Commit

--- COMP123-S2016-Lab3/Program.cs
+++ COMP123-S2016-Lab3/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ * Author : Dilakshan Packiyanathan
+ * Student Number : 300843256
 
-namespace COMP123_S2016_Lab3
+ */
+namespace COMP123_Lab3
 {
-    class Program
+    /**
+     * This class is the "driver" class for our Program
+     * 
+     * @class Program
+     */
+    public class Program
     {
-        static void Main(string[] args)
+
+        /**
+         * the main method for our driver class Program
+         * 
+         * @method Main
+         * @param {string[]} args
+         */
+        public static void Main(string[] args)
         {
+        
+
+            Deck deck = new Deck(); // create a deck of cards
+            deck.Display(); // display the initial state of the deck
+
+            deck.Shuffle(); // shuffle the deck
+            deck.Display(); // display shuffled deck
+            
+
+            Card cardDealt = deck.Deal();
+            Console.WriteLine("Card dealt: {0} of {1}", cardDealt.Face, cardDealt.Suit);
+            Console.WriteLine();
+            deck.Display();
+
+
         }
+
+
     }
 }
